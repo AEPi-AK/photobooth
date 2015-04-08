a7b0c2974164d229cb49f89d6f0f2397203698e1
Avi Romanoff <avi@romanoff.me>
2015-04-08 01:07:54 -0400

    SMILE!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from facepy import GraphAPI
-import requests
+import picamera
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -8,29 +8,16 @@ with open('config.json') as config_file:
     page_id = config['page_id']
     access_token = config['access_token']
 
+camera = picamera.PiCamera()
 graph = GraphAPI(access_token)
 # album = graph.get_object(album_id)
 # print album
 
-# url = 'http://httpbin.org/post'
-# multiple_files = [('images', ('foo.png', open('foo.png', 'rb'), 'image/png')),
-#                   ('images', ('bar.png', open('bar.png', 'rb'), 'image/png'))]
-# r = requests.post(url, files=multiple_files)
-
 path = "%s/photos" % page_id
-r = graph.post(path, source=open('test.png'))
-print r
 
-# posts = graph.get_connections(profile['id'], 'posts')
-#
-# while True:
-#     try:
-#         # Perform some action on each post in the collection we receive from
-#         # Facebook.
-#         [some_action(post=post) for post in posts['data']]
-#         # Attempt to make a request to the next page of data, if it exists.
-#         posts = requests.get(posts['paging']['next']).json()
-#     except KeyError:
-#         # When there are no more pages (['paging']['next']), break from the
-#         # loop and end the script.
-#         break
+while True:
+    IMG_NAME = "snap.png"
+    raw_input("HIT ME")
+    camera.capture(IMG_NAME)
+    r = graph.post(path, source=open(IMG_NAME))
+    print r
