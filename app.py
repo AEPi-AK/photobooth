d026c97de1d0b07041ad7be2bd127c621670c14b
Avi Romanoff <avi@romanoff.me>
2015-04-16 13:12:41 -0400

    GPIO #3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import urllib
 import facepy
 import logging
@@ -10,11 +11,16 @@ logger = logging.getLogger('photobooth')
 coloredlogs.install(level=logging.DEBUG)
 
 if os.uname()[4][:3] == 'arm':
-    logging.warning("ARM archiecture detected; setting up picamera...")
+    logging.warning("ARM archiecture detected; setting up picamera & GPIO...")
+    IS_PI = True
     import picamera
+    import RPi.GPIO as GPIO
     camera = picamera.PiCamera()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 else:
-    logging.warning("Non-ARM archiecture detected; skipping picamera setup")
+    IS_PI = False
+    logging.warning("Non-ARM archiecture detected; skipping picamera & GPIO setup")
 
 with open('config.json') as config_file:
     logging.info("Parsing config file..."),
@@ -28,10 +34,18 @@ graph = facepy.GraphAPI(access_token)
 path = "%s/photos" % page_id
 
 def takePictureAndUploadIt():
+    time.sleep(0.1) # Rate limiting
     img_name = "snap-%s.jpg" % datetime.datetime.now().isoformat()
     camera.capture(img_name)
     r = graph.post(path, source=open(img_name))
 
-while True:
-    if raw_input("\nTake picture? ['n' to bail] ") == "n": break
-    takePictureAndUploadIt()
+if IS_PI:
+    while True:
+        input_state = GPIO.input(3)
+        if input_state == False:
+            logging.debug('Button pressed!!')
+            takePictureAndUploadIt()
+else:
+    while True:
+        if raw_input("\nTake picture? ['n' to bail] ") == "n": break
+        takePictureAndUploadIt()
